5a64bde515663aa2fd081af9629c6baeb761d87f
Robert Zaremba <robert.zaremba@scale-it.pl>
2017-02-06 22:09:37 +0100

    added tests for clshelpers

--- tests/test_clshelpers.py
+++ tests/test_clshelpers.py
@@ -10,7 +10,21 @@ class X:
         pass
 
 
+class X2:
+    @lazy_init
+    def __init__(self, a1, a2, a3=False):
+        pass
+
+    def m(self):
+        pass
+
+
 def test_lazy_init():
     x = X('p1', 'p2', 'extra', check=True)
-
     assert vars(x) == {'a1': 'p1', 'a2': 'p2'}
+
+    x2 = X2('p1', 'p2')
+    assert vars(x2) == {'a1': 'p1', 'a2': 'p2'}
+
+    x2 = X2('p1', 'p2', True)
+    assert vars(x2) == {'a1': 'p1', 'a2': 'p2', 'a3': True}
